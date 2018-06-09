8c5ccb7cad17d0aad78c3df684526086f5477c7c
Keith <hnefatl@gmail.com>
2018-06-09 17:01:03 +0100

    Added colours

--- app/World.hs
+++ app/World.hs
@@ -31,7 +31,7 @@ data Particle = Particle
 data World = World
     {
         particles :: ![Particle],
-        getIntensity :: G.Point -> G.Point -> Float,
+        getIntensity :: Particle -> Particle -> Float,
         circleRadius :: !Float,
         dimensions :: !WindowDimensions
     }
@@ -51,18 +51,21 @@ render w = originTransform (floatDimensions w) $ G.pictures $ particlesP ++ conn
         makePoint p = G.color (colour p) $ translate (pos p) $ G.circleSolid (circleRadius w)
         particlesP = map makePoint $ particles w
         -- Construct line pictures
-        makeLine p1 p2 i = G.color (G.withAlpha i G.white) $ G.line [p1, p2]
-        connectionsP = map (\(p1, p2) -> makeLine p1 p2 (255 * getIntensity w p1 p2)) (getPairs $ points w)
+        connectionsP = map (makeLine w) (getPairs $ particles w)
     
+makeLine :: World -> (Particle, Particle) -> G.Picture
+makeLine w (p1, p2) = G.color (G.withAlpha i avgColour) $ G.line [pos p1, pos p2]
+    where
+        avgColour = G.mixColors 0.5 0.5 (colour p1) (colour p2)
+        i = 255 * getIntensity w p1 p2
+
 -- Get all unique pairs: [1,2,3] -> [(1,2), (1,3), (2,3)]
 -- Used to increase efficiency when constructing pictures, as we don't consider the same pair twice
 getPairs :: [a] -> [(a, a)]
 getPairs l = [ (x, y) | (x:t) <- tails l, y <- t ]
 
-
 worldStep :: Float -> World -> World
 worldStep dt w = w { particles = map (updateParticle dt w) (particles w) }
-
 
 -- Bounce particles off the window edges, update positions according to velocities
 updateParticle :: Float -> World -> Particle -> Particle
@@ -84,11 +87,12 @@ defaultWorld d = do
         ps <- replicateM particleCount (newRandomParticle d)
         return World { particles = ps, getIntensity = intensity, circleRadius = particleRadius, dimensions = d }
     where
-        intensity p1 p2 = 1 / (magV (p1 - p2))**1.6
+        intensity p1 p2 = 1 / (magV (pos p1 - pos p2))**1.6
 
 newParticle :: G.Point -> IO Particle
-newParticle position = Particle position <$> newDir <*> pure G.white
+newParticle position = Particle position <$> newDir <*> col
     where newDir = (,) <$> randomRIO (-20, 20) <*> randomRIO (-20, 20)
+          col = G.makeColor <$> randomRIO (0,1) <*> randomRIO (0,1) <*> randomRIO (0,1) <*> pure 1
 
 newRandomParticle :: WindowDimensions -> IO Particle
 newRandomParticle (width, height) = newPoint >>= newParticle
